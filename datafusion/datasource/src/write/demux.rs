d9d440d6c2b1a629a3a36db5690403ff6caca07d
younghun1.jung <younghun1.jung@lgepartner.com>
2025-03-25 00:15:53 +0000

    update cargo.toml

--- datafusion/datasource/src/write/demux.rs
+++ datafusion/datasource/src/write/demux.rs
@@ -117,7 +117,7 @@ pub(crate) fn start_demuxer_task(
     } else {
         // There could be an arbitrarily large number of parallel hive style partitions being written to, so we cannot
         // bound this channel without risking a deadlock.
-        info!("config.original_url: {}", config.original_url);
+        println!("config.original_url: {}", config.original_url);
         let partition_by = config.table_partition_cols.clone();
         let keep_partition_by_columns = config.keep_partition_by_columns;
         SpawnedTask::spawn(async move {
